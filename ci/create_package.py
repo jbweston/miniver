d969438517541a484e2f0a5ea5fb6550551298e4
Joseph Weston <joseph@weston.cloud>
2019-10-17 22:13:47 +0200

    blackify files

--- ci/create_package.py
+++ ci/create_package.py
@@ -1,13 +1,11 @@
 from pathlib import Path
 from shutil import copyfile
 
-Path('my_package').mkdir(exist_ok=True)
-copyfile('../miniver/miniver/_static_version.py',
-         'my_package/_static_version.py')
-copyfile('../miniver/miniver/_version.py',
-         'my_package/_version.py')
+Path("my_package").mkdir(exist_ok=True)
+copyfile("../miniver/miniver/_static_version.py", "my_package/_static_version.py")
+copyfile("../miniver/miniver/_version.py", "my_package/_version.py")
 
-README_filename = '../miniver/README.md'
+README_filename = "../miniver/README.md"
 
 
 def write_snippet_from_readme(outfile, start_marker, file_header=None):
@@ -17,22 +15,21 @@ def write_snippet_from_readme(outfile, start_marker, file_header=None):
             if line.startswith(start_marker):
                 break
         else:
-            raise RuntimeError('Could not find start_marker: {}'
-                               ''.format(start_marker))
-        with open(outfile, 'w') as out:
+            raise RuntimeError(
+                "Could not find start_marker: {}" "".format(start_marker)
+            )
+        with open(outfile, "w") as out:
             out.write(line)
             if file_header is not None:
                 out.write(file_header)
             for line in f:
-                if line.startswith('```'):
+                if line.startswith("```"):
                     break
                 out.write(line)
 
 
-write_snippet_from_readme("setup.py",
-                          "# Your project's setup.py",
-                          "from setuptools import setup\n")
-write_snippet_from_readme(".gitattributes",
-                          "# Your project's .gitattributes")
-write_snippet_from_readme("my_package/__init__.py",
-                          "# Your package's __init__.py")
+write_snippet_from_readme(
+    "setup.py", "# Your project's setup.py", "from setuptools import setup\n"
+)
+write_snippet_from_readme(".gitattributes", "# Your project's .gitattributes")
+write_snippet_from_readme("my_package/__init__.py", "# Your package's __init__.py")
