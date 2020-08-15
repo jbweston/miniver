1ec59e4ceee260c16b768f547b7d849b24f48694
Joseph Weston <joseph@weston.cloud>
2020-08-15 14:30:53 -0700

    test against Python 3.8 also

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
