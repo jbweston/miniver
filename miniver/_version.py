0f081c2505cf1497693d6758541cd537e933ad31
Mark Harfouche <mark.harfouche@gmail.com>
2022-10-16 08:58:59 -0400

    Use a single call to git to find if it is dirty

--- miniver/_version.py
+++ miniver/_version.py
@@ -66,7 +66,7 @@ def get_version_from_git():
     for opts in [["--first-parent"], []]:
         try:
             p = subprocess.Popen(
-                ["git", "describe", "--long", "--always"] + opts,
+                ["git", "describe", "--long", "--always", "--dirty"] + opts,
                 cwd=package_root,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
@@ -81,13 +81,17 @@ def get_version_from_git():
     description = (
         p.communicate()[0]
         .decode()
-        .strip("v")  # Tags can have a leading 'v', but the version should not
+        .lstrip("v")  # Tags can have a leading 'v', but the version should not
         .rstrip("\n")
-        .rsplit("-", 2)  # Split the latest tag, commits since tag, and hash
+        .rsplit("-")  # Split the latest tag, commits since tag, and hash
     )
 
+    # We could be left with 3, or 4 parts to git describe at this point
+    # version dev git hash [dirty]
+    # Where dirty is an optional part
+
     try:
-        release, dev, git = description
+        release, dev, git = description[:3]
     except ValueError:  # No tags, only the git hash
         # prepend 'g' to match with format returned by 'git describe'
         git = "g{}".format(*description)
@@ -100,13 +104,8 @@ def get_version_from_git():
     else:
         labels.append(git)
 
-    try:
-        p = subprocess.Popen(["git", "diff", "--quiet"], cwd=package_root)
-    except OSError:
-        labels.append("confused")  # This should never happen.
-    else:
-        if p.wait() == 1:
-            labels.append("dirty")
+    if description[-1] == "dirty":
+        labels.append("dirty")
 
     return Version(release, dev, labels)
 
